8bf2403150b7c53865ca9dff45853b03c486ee2f
Jose Inacio <joseinacio.dev@gmail.com>
2026-09-24 18:14:09 -0300

    Desenvolvendo os serviços

--- src/main/java/com/pedidos/service/PedidoService.java
+++ src/main/java/com/pedidos/service/PedidoService.java
@@ -1,4 +1,18 @@
 package com.pedidos.service;
 
+import com.pedidos.model.Cliente;
+import com.pedidos.model.Pedido;
+import com.pedidos.repository.PedidoDao;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class PedidoService {
+    private final ClienteService clienteService;
+    private final PedidoDao pedidoDao;
+
+    public Pedido criarPedido (int clienteId){
+        Cliente cliente = clienteService.buscarPorId(clienteId);
+        Pedido pedido = new Pedido(cliente);
+        return pedidoDao.salvar(pedido);
+    }
 }
--- src/main/java/com/pedidos/service/ProdutoService.java
+++ src/main/java/com/pedidos/service/ProdutoService.java
@@ -1,4 +1,51 @@
 package com.pedidos.service;
 
+import com.pedidos.exception.DadoInvalidoException;
+import com.pedidos.exception.EstoqueInsuficienteException;
+import com.pedidos.exception.ProdutoNaoEncontradoException;
+import com.pedidos.model.Produto;
+import com.pedidos.repository.ProdutoDao;
+import com.pedidos.util.ValidadorUtil;
+import jakarta.validation.ConstraintViolation;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Set;
+
+@RequiredArgsConstructor
 public class ProdutoService {
+
+    private final ProdutoDao produtoDao;
+
+    public Produto cadastrar(Produto produto) {
+
+        Set<ConstraintViolation<Produto>> violacoes = ValidadorUtil.getValidator().validate(produto);
+        if (!violacoes.isEmpty()) {
+            throw new DadoInvalidoException("");
+        }
+        return produtoDao.cadastrar(produto);
+    }
+
+    public Produto buscarPorId(int id) {
+        Produto produto = produtoDao.buscarPorId(id);
+        if (produto == null) {
+            throw new ProdutoNaoEncontradoException("");
+        }
+        return produto;
+    }
+
+    public Produto atualizarEstoque(int produtoId, int quantidade) {
+        Produto buscando = buscarPorId(produtoId);
+
+        int novaQuantidade = buscando.getQuantidadeEmEstoque() + quantidade;
+
+        if (novaQuantidade < 0) {
+            throw new EstoqueInsuficienteException("");
+        }
+        return produtoDao.atualizarEstoque(produtoId, quantidade);
+    }
+
+    public void deletar(int id){
+        buscarPorId(id);
+        produtoDao.deletar(id);
+    }
 }
